7c91c7fb79e0eb03f872fac1b93047ce253f6b35
Krasimir Tsonev <krasimir@outset.ws>
2015-09-12 20:16:12 +0300

    Generating csssteal.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
  
-gulp.task('compress', function() {
+gulp.task('default', function() {
   return gulp.src('src/*.js')
     .pipe(uglify())
+    .pipe(gulp.dest('lib'))
+    .pipe(rename('csssteal.min.js'))
     .pipe(gulp.dest('lib'));
 });
